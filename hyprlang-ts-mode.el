14a0c422963577636e43aa6a84db78ff569c42ef
Nathan Abeje <nathanmelkau@gmail.com>
2024-12-22 16:08:42 +0100

    font lock and indentation fix.
    - make font lock queries the same as the ones shown in the language
    grammer definition.
    - make indentation queries the simpler.

--- hyprlang-ts-mode.el
+++ hyprlang-ts-mode.el
@@ -25,79 +25,87 @@
   :safe 'integerp
   :group 'hyprlang)
 
-(defvar hyprlang-ts-font-lock-rules
-  '(;; Hyperlang font locking rules
+(defvar hyprlang-ts-mode--font-lock-rules
+  ;; Hyperlang font locking rules
+  (treesit-font-lock-rules
 
-    :language hyprlang
-    :override t
-    :feature comment
-    ((comment) @font-lock-comment-face)
+   :language 'hyprlang
+   :feature 'comment
+   `((comment) @font-lock-comment-face)
 
-    :language hyprlang
-    :override t
-    :feature section
-    ((section (name) @font-lock-type-face))
+   :language 'hyprlang
+   :feature 'special
+   `([ "source" "exec" "exec-once" ] @font-lock-keyword-face)
 
-    :language hyprlang
-    :override t
-    :feature assignment
-    ((assignment (name) @font-lock-builtin-face "=" @font-lock-operator-face))
+   :language 'hyprlang
+   :feature 'keyword
+   `((keyword (name) @font-lock-keyword-face))
 
-    :language hyprlang
-    :override t
-    :feature keyword
-    ((keyword (name) @font-lock-keyword-face "=" @font-lock-operator-face))
+   :language 'hyprlang
+   :feature 'assignment
+   `((assignment (name) @font-lock-property-use-face))
 
-    :language hyprlang
-    :override t
-    :feature exec
-    ((exec "exec" @font-lock-keyword-face "=" @font-lock-operator-face)
-     (exec "exec-once" @font-lock-keyword-face "=" @font-lock-operator-face))
+   :language 'hyprlang
+   :override t
+   :feature 'section
+   `((section (name) @font-lock-type-face))
 
-    :language hyprlang
-    :override t
-    :feature source
-    ((source "source" @font-lock-keyword-face "=" @font-lock-operator-face))
+   :language 'hyprlang
+   :override t
+   :feature 'section-device
+   `((section device: (device_name) @font-lock-type-face))
 
-    :language hyprlang
-    :override t
-    :feature variable
-    ((variable) @font-lock-variable-name-face)
+   :language 'hyprlang
+   :feature 'variable
+   `((variable) @font-lock-variable-name-face)
 
-    :language hyprlang
-    :override t
-    :feature string
-    ((string) @font-lock-string-face)
+   :language 'hyprlang
+   :feature 'dollar
+   `("$" @font-lock-punctuation-face)
 
-    :language hyprlang
-    :override t
-    :feature string_literal
-    ((string_literal) @font-lock-string-face)
+   :language 'hyprlang
+   :feature 'boolean
+   `((boolean) @font-lock-number-face)
 
-    :language hyprlang
-    :override t
-    :feature number
-    ((number) @font-lock-number-face)
+   :language 'hyprlang
+   :feature 'mod
+   `((mod) @font-lock-constant-face)
 
-    :language hyprlang
-    :override t
-    :feature boolean
-    ((boolean) @font-lock-number-face)
+   :language 'hyprlang
+   :feature 'builtin
+   `([ "rgb" "rgba" ] @font-lock-builtin-face)
 
-    :language hyprlang
-    :override t
-    :feature mod
-    ((mod) @font-lock-variable-use-face)))
+   :language 'hyprlang
+   :feature 'number
+   `([ (number) (legacy_hex) (angle) (hex) (display) (position) ] @font-lock-number-face)
+
+   :language 'hyprlang
+   :feature 'deg
+   `("deg" @font-lock-type-face)
+
+   :language 'hyprlang
+   :feature 'delimit
+   `([ "," ":" ] @font-lock-delimiter-face)
+
+   :language 'hyprlang
+   :feature 'bracket
+   `([ "(" ")" "{" "}" ] @font-lock-bracket-face)
+
+   :language 'hyprlang
+   :feature 'operator
+   `([ "=" "-" "+" ] @font-lock-operator-face)
+
+   :language 'hyprlang
+   :feature 'string
+   `([ (string) (string_literal) ] @font-lock-string-face)))
 
 (defvar hyprlang-ts-mode--indent-rules
   ;; Hyprlang indentation rules
   `((hyprlang
-     ((node-is "}") parent 0)
-     ((parent-is "section") parent hyprlang-ts-mode-indent-offset)
-     ((node-is "section") parent 0)
-     ((node-is "comment") parent 0)
-     ((node-is ,(regexp-opt '("assignment" "keyword" "exec" "declaration"))) prev-sibling 0)
-     (no-node parent 0))))
+     ((node-is "}") parent-bol 0)
+     ((parent-is "configuration") column-0 0)
+     ((parent-is "comment") parent-bol 1)
+     ((parent-is "section") parent-bol hyprlang-ts-mode-indent-offset))))
 
 (defvar hyprlang-ts-mode--syntax-table
   (let ((syntax-table (make-syntax-table)))
@@ -110,19 +118,17 @@
   "Setup treesit for hyprlang.
 This function is the core of the`hyprlang-ts-mode'.
 it sets up font locking and indentation rules."
+  (treesit-parser-create 'hyprlang)
   ;; comment starts with # and it doesn't need and end symbol
   (setq-local comment-start "#")
   (setq-local comment-end "")
 
   ;; set font lock
-  (setq-local treesit-font-lock-settings
-              (apply #'treesit-font-lock-rules
-                     hyprlang-ts-font-lock-rules))
-  (setq-local font-lock-defaults nil)
+  (setq-local treesit-font-lock-settings hyprlang-ts-mode--font-lock-rules)
   (setq-local treesit-font-lock-feature-list
-              '((comment)
-                (section assignment keyword exec source declaration)
-                (variable string string_literal number boolean mod)))
+              '((comment special deg delimit dollar bracket)
+                (section section-device assignment keyword declaration)
+                (builtin operator variable string number boolean mod)))
 
   ;; set indentation rules
   (setq-local treesit-simple-indent-rules hyprlang-ts-mode--indent-rules)
@@ -132,8 +138,10 @@ it sets up font locking and indentation rules."
   "A mode for editing Hyprland configuration file."
   :group 'hyprlang
   :syntax-table hyprlang-ts-mode--syntax-table
+
   (unless (treesit-ready-p 'hyprlang)
     (error "Tree-sitter for hyprlang isn't available"))
+
   (hyprlang-ts-setup))
 
 (if (treesit-ready-p 'hyprlang)
